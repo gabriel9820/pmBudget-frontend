8cdfdae012e9748880a3859745cf9a2eaa46b69a
Gabriel Alves <gabriel_alves_44@live.com>
2022-04-13 22:09:35 -0300

    feat: changed api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://localhost:7047/api",
+  baseURL: "http://localhost:5047/api",
 });
--- src/utils/exception-handler.ts
+++ src/utils/exception-handler.ts
@@ -1,8 +1,7 @@
 export const apiExceptionHandler = (error: any) => {
   const message =
     error?.response?.data?.message || error?.message || "Ocorreu um erro";
-
   const errors = error?.response?.data?.errors || undefined;
 
-  console.log({ message, errors });
+  alert(JSON.stringify({ message, errors }));
 };
