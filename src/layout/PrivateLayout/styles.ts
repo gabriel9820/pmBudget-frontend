83158254f04530fc64ebc1540fdc7b5b55e7bf96
Gabriel Alves <gabriel_alves_44@live.com>
2022-04-30 16:48:23 -0300

    feat: padding private layout

--- src/layout/PrivateLayout/styles.ts
+++ src/layout/PrivateLayout/styles.ts
@@ -7,5 +7,5 @@ interface IProps {
 export const Content = styled.div<IProps>`
   margin-left: ${(props) => (props.isMenuOpen ? "17rem" : "5rem")};
   transition: margin-left 0.4s;
-  padding: 3rem;
+  padding: 5rem 10rem;
 `;
